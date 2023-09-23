15f5cc82c2df8da968b03ac68920c78035eda4f5
Sana Assim <sanaassim1@gmail.com>
2023-09-23 05:32:38 -0700

    Client configuration file with Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Puppet manifest to configure SSH client
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  ensure => present,
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => present,
+}
